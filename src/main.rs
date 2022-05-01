2e560b2946ec2ceb63f9c6790b0b753a9ee47df2
ajosemalik <ajose.malik@gmail.com>
2022-05-01 08:08:10 -0400

    add initial structs and implement Bills

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+
+struct Bill {
+    name: String,
+    amount: f64,
+}
+
+struct Bills {
+    inner: Vec<Bill>,
+}
+
+impl Bills {
+    fn new() -> Self {
+        Self { inner: vec![] }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    return;
 }
