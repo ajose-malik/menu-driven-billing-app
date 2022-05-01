4f6742a8044ead4255a2521b060eae25e6ecd375
ajosemalik <ajose.malik@gmail.com>
2022-05-01 08:27:20 -0400

    update Bills implementation and create get_input function

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,25 @@ impl Bills {
     fn new() -> Self {
         Self { inner: vec![] }
     }
+
+    fn add(&mut self, bill:Bill) {
+        self.inner.push(bill)
+    }
+
+    fn get_all(&self) -> &Vec<Bill> {
+        &self.inner
+    }
 }
+
+fn get_input () -> String {
+    let mut buffer = String::new();
+    while io::stdin().read_line(&mut buffer).is_err() {
+        println!("Please enter your data again");
+    }
+
+    buffer.trim().to_owned()
+}
+
 
 fn main() {
     return;
